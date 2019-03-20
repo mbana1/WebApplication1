41461e34510adfa88434d209496f46b1ec0d87b4
mbana1 <mbana1@yahoo.com>
2019-03-20 16:51:20 +0100

    Ajoutez des fichiers projet.

---
+++ WebApplication1/Controllers/OperationController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using System.Data.SqlClient;
+
+
+namespace WebApplication1.Controllers
+{
+    public class OperationController : Controller
+    {
+        // GET: Operation
+        public ActionResult Addition(float operandeGauche, float operandeDroite)
+        {
+            
+            Operation operationtest = new Operation(operandeGauche,"+", operandeDroite);
+           
+            EnregistrerOperation(operandeGauche, "+", operandeDroite);
+
+
+            return View(operationtest);
+        }
+
+
+
+        public string Soustraction(float operandeGauche, float operandeDroite)
+        {
+
+            Operation operationtest = new Operation(operandeGauche, "-", operandeDroite);
+
+            EnregistrerOperation(operandeGauche, "-", operandeDroite);
+
+
+            return "le résultat de l'oppération est  :" + operationtest.GetResultat() ;
+        }
+        public string Multiplication(float operandeGauche, float operandeDroite)
+        {
+
+            Operation operationtest = new Operation(operandeGauche, "*", operandeDroite);
+
+            EnregistrerOperation(operandeGauche, "*", operandeDroite);
+
+
+            return "le résultat de l'oppération est  :" + operationtest.GetResultat();
+        }
+        public string Division(float operandeGauche, float operandeDroite)
+        {
+
+            Operation operationtest = new Operation(operandeGauche, "/", operandeDroite);
+
+            EnregistrerOperation(operandeGauche, "/", operandeDroite);
+
+
+            return "le résultat de l'oppération est  :" + operationtest.GetResultat();
+        }
+        public string Exposant(float operandeGauche, float operandeDroite)
+        {
+
+            Operation operationtest = new Operation(operandeGauche, "^", operandeDroite);
+
+            EnregistrerOperation(operandeGauche, "^", operandeDroite);
+
+
+            return "le résultat de l'oppération est  :" + operationtest.GetResultat();
+        }
+        const string cheminBase = @"Server=.\sqlexpress;Initial Catalog = Calculatrice; Integrated Security = True";
+        public static void EnregistrerOperation(double operandedeGauche, string operateur, double operandedroite)
+        {
+            SqlConnection connection = new SqlConnection(cheminBase);
+
+
+
+            connection.Open();
+
+            SqlCommand command = connection.CreateCommand();
+
+            command.CommandText = "INSERT INTO Operation(OperandeGauche,Operateur, OperandeDroite) VALUES (@operandedeGauche,@operateur,@operandedroite)";
+            command.Parameters.AddWithValue("@operandedeGauche", operandedeGauche);
+            command.Parameters.AddWithValue("@operateur", operateur);
+            command.Parameters.AddWithValue("@operandedroite", operandedroite);
+
+            command.ExecuteNonQuery();
+            connection.Close();
+            command.Parameters.Clear();
+        }
+        public string NombreOperation()
+        {
+            SqlConnection connection = new SqlConnection(cheminBase);
+
+
+
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("SELECT COUNT(*) FROM Operation", connection);
+            int NombreOperation = (int)command.ExecuteScalar();
+
+            //command.ExecuteNonQuery();
+            connection.Close();
+            command.Parameters.Clear();
+            return "Vous avez effectuer : " + NombreOperation + "  opérations";
+
+        }
+
+        public static void CalculeRealiser()
+        {
+            SqlConnection connection = new SqlConnection(cheminBase);
+
+
+
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("SELECT OperandeGauche,Operateur,OperandeDroite FROM Operation", connection);
+            SqlDataReader datareader = command.ExecuteReader();
+            List<Operation> ListeDesOperations = new List<Operation>();
+            while (datareader.Read())
+            {
+                Operation OperationsEfectuee = new Operation(datareader.GetFloat(0), datareader.GetString(1), datareader.GetFloat(2));
+                
+
+                ListeDesOperations.Add(OperationsEfectuee);
+
+                /*Console.WriteLine("vous avez effectué les opérations suivante  :" + PGauche + "  " + Poperation + "  " + Pdroite); Console.WriteLine("\n")*/
+                ;
+
+            }
+            foreach (Operation operations in ListeDesOperations)
+            {
+
+
+               
+
+
+
+
+            }
+            //command.ExecuteNonQuery();
+            connection.Close();
+            command.Parameters.Clear();
+
+
+        }
+    }
+}
---
+++ WebApplication1/Models/Operation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+  public  class Operation
+    {
+        public float OperandeGauche { get; private set; }
+        public string Operateur { get; private set; }
+        public float OperandeDroite { get; private set; }
+
+        public Operation(float operandeGauche, string operateur, float operandeDroite)
+        {
+            this.OperandeGauche = operandeGauche;
+            this.Operateur = operateur;
+            this.OperandeDroite = operandeDroite;
+        }
+
+        public float GetResultat()
+        {
+            switch (Operateur)
+            {
+                case "+":
+                    return OperandeGauche + OperandeDroite;
+                case "-":
+                    return OperandeGauche - OperandeDroite;
+                case "*":
+                    return OperandeGauche * OperandeDroite;
+                case "/":
+                    return OperandeGauche / OperandeDroite;
+                case "^":
+                    return (float)Math.Pow(OperandeGauche, OperandeDroite);
+            }
+            throw new Exception("Opérateur non reconnu");
+        }
+
+        public bool IsValide()
+        {
+            switch (Operateur)
+            {
+                case "/":
+                    return OperandeDroite != 0.0f;
+                case "^":
+                    return OperandeDroite >= 0.0f;
+                default:
+                    return true;
+            }
+        }
+    }
+}
